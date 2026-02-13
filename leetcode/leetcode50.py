ae3e35dfe79390076ce46d0c81721218d73ef78d
Nishit Patel <patelnishitkumarbsc24@rvu.edu.in>
2026-02-13 09:46:23 +0530

    Update leetcode50.py

--- leetcode/leetcode50.py
+++ leetcode/leetcode50.py
@@ -1,23 +1,17 @@
-class Solution {
-public:
-    double myPow(double x, int n) {
-        long long N = n;   
+class Solution(object):
+    def myPow(self, x, n):
+        N = n
         
-        if (N < 0) {
-            x = 1 / x;
-            N = -N;
-        }
+        if N < 0:
+            x = 1 / x
+            N = -N
         
-        double result = 1.0;
+        result = 1.0
         
-        while (N > 0) {
-            if (N % 2 == 1) {
-                result *= x;
-            }
-            x *= x;
-            N /= 2;
-        }
+        while N > 0:
+            if N % 2 == 1:
+                result *= x
+            x *= x
+            N //= 2
         
-        return result;
-    }
-};
+        return result
